9002f98184cd65580cd01266bad82475f0351f16
Francisco Soares <chicaoiskemia@gmail.com>
2018-05-01 22:30:47 -0300

    Add fetch in example
    - Shifts now include a relationship to a Hospital.
    - The example now includes fetching of records (Shift, Doctor and Hospital) as well as explicitly saving the context at the end of viewDidLoad. That may not be necessary on a day-to-day basis, since the context was already saved when the app terminated, but for quick testing in the simulator is good - for running the app in the simulator might not call AppDelegate.applicationWillTerminate.

--- CoreDataTeste2/ViewController.swift
+++ CoreDataTeste2/ViewController.swift
@@ -7,37 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        let doctor = Doctor(context: context)
-        doctor.name = "Juliana"
-        doctor.school = "UFPE"
-        doctor.crm = "1234567890"
+        let shiftRequest = NSFetchRequest<Shift>(entityName: "Shift")
+        let shifts = try! context.fetch(shiftRequest)
+        if !shifts.isEmpty {
+            for shift in shifts{
+                print(shift)
+            }
+        }
+        else{
+            let doctorRequest = NSFetchRequest<Doctor>(entityName: "Doctor")
+            let doctors : [Doctor] = try! context.fetch(doctorRequest)
+            let doctor : Doctor
+            if doctors.isEmpty {
+                doctor = Doctor(context: context)
+                doctor.name = "Juliana"
+                doctor.school = "UFPE"
+                doctor.crm = "1234567890"
+            }
+            else {
+                doctor = doctors.first!
+            }
+            let hospitalRequest = NSFetchRequest<Hospital>(entityName: "Hospital")
+            let hospitals = try! context.fetch(hospitalRequest)
+            
+            let hospital : Hospital = hospitals.first ??  Hospital(name: "HC", address: "av. Professor Moraes Rego" , context: context)
+            
+            let sh = Shift(context: context)
+            sh.doctor = doctor
+            sh.duration = 12
+            sh.hospital = hospital
         
-        let hospital = Hospital(name: "HC", address: "av. Jorn..." , context: context)
-        //        hospital.name = "HC"
-        //        hospital.address = "av. Jornalista Arthur Sá"
-        
-        let du = Shift(context: context)
-        du.doctor = doctor
-        du.duration = 12
-        
-        
+            print(sh)
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+        }
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
 
